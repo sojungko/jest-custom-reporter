32d7fb5694421e104601b01cde03d29a8df41692
Sojung Park <sojungko@gmail.com>
2018-04-03 20:27:07 -0400

    tests log as expected, need styling

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,9 @@ module.exports = {
   "extends": ["standard", "airbnb"],
   "rules": {
     "class-methods-use-this": ["off"],
+    "func-names": ["as-needed"],
+    "no-param-reassign": ["off"],
+    "no-plusplus": ["off"],
     "no-underscore-dangle": ["error", { "allowAfterThis": true }],
     "semi": ["error", "always"],
   }
--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const {
   red,
   gray,
   cyan,
+  white,
 } = require('chalk');
 
 const {
   testDuration,
+  symbols,
 } = require('./lib/utils');
 
 const { log } = console;
@@ -17,24 +19,70 @@ class JestCustomReporter {
     this._options = options;
   }
 
+  onRunStart({ numTotalTestSuites }) {
+    log();
+    log(gray(`Found ${numTotalTestSuites} test suites`));
+  }
+
+  /**
+ *
+ * @param {String} currentTitle
+ * @param {Array} testResults
+ * @param {Number} index
+ * @param {Number} level
+ */
+  recursivelyReport(prevTitle, testResults, resultsIndex, titlesIndex) {
+    if (!testResults[resultsIndex]) {
+      return;
+    }
+    const { ancestorTitles, status, title } = testResults[resultsIndex];
+    // log('ancestorTitles', ancestorTitles);
+    const currentTitle = ancestorTitles[titlesIndex];
+    // log('prevTitle', prevTitle);
+    // log('currentTitle', currentTitle);
+    // log('resultsIndex', resultsIndex);
+    // log('titlesIndex', titlesIndex);
+
+    if (!ancestorTitles[titlesIndex]) {
+      log(symbols(status), gray(title));
+      this.recursivelyReport(currentTitle, testResults, ++resultsIndex, titlesIndex);
+      return;
+    }
+    if (prevTitle !== currentTitle) {
+      log(white(currentTitle));
+      log(symbols(status), gray(title));
+      this.recursivelyReport(currentTitle, testResults, ++resultsIndex, ++titlesIndex);
+      titlesIndex--;
+    } else {
+      log(symbols(status), gray(title));
+      this.recursivelyReport(currentTitle, testResults, ++resultsIndex, titlesIndex);
+    }
+  }
+
   onRunComplete(test, results) {
     const {
       numFailedTests,
       numPassedTests,
       numPendingTests,
       testResults,
-      numTotalTests,
+      // numTotalTests,
       startTime,
     } = results;
 
-    testResults.map(({ failureMessage }) => {
+    testResults.forEach(({ testFilePath, testResults, failureMessage }) => {
+      // log(`Results for running tests on ${testFilePath}`);
+      this.recursivelyReport(testFilePath, testResults, 0, 0);
+
       if (failureMessage) {
         log(failureMessage);
       }
+      log();
     });
 
     if (numPassedTests) {
-      log(green(`${numPassedTests} passing`), gray(`(${testDuration(new Date(), new Date(startTime))})`));
+      const end = new Date();
+      const start = new Date(startTime);
+      log(green(`${numPassedTests} passing`), gray(`(${testDuration(end, start)})`));
     }
     if (numFailedTests) {
       log(red(`${numFailedTests} failing`));
@@ -43,24 +91,15 @@ class JestCustomReporter {
       log(cyan(`${numPendingTests} pending`));
     }
   }
-
-  // onTestResult(test, {testResults}) {
-  //   testResults.map((result) => {
-  //     const {title, duration, status, ancestorTitles} = result;
-  //     const head = `${ancestorTitles.join(' > ')} >`;
-  //     log(
-  //       `    ${this._getStatus(status)} ${headFmt(head)} ${titleFmt(title)} ${durationFmt('(' + duration + 'ms)')}`
-  //     );
-  //   });
-  // }
 }
 
-exports.symbols = {
-  ok: '✓',
-  err: '✖',
-  dot: '․',
-  comma: ',',
-  bang: '!',
-};
+// symbols = {
+//   passed: '✓',
+//   pending: '-',
+//   err: '✖',
+//   dot: '․',
+//   comma: ',',
+//   bang: '!',
+// };
 
 module.exports = JestCustomReporter;
